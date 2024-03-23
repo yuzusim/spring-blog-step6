158356e2c00a63b01788967a665c5bf8ec4780c9
yuzusim <yuzusim@gmail.com>
2024-03-23 13:34:03 +0900

    환경설정셋팅완료

--- src/main/java/shop/mtcoding/blog/board/Board.java
+++ src/main/java/shop/mtcoding/blog/board/Board.java
@@ -2,9 +2,12 @@ package shop.mtcoding.blog.board;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.sql.Timestamp;
 
+@NoArgsConstructor
 @Data
 @Table(name = "board_tb")
 @Entity
@@ -15,6 +18,13 @@ public class Board {
     private String title;
     private String content;
     private String username;
+
+    @CreationTimestamp
     private Timestamp createdAt;
 
+    public Board(String title, String content, String username) {
+        this.title = title;
+        this.content = content;
+        this.username = username;
+    }
 }
